1af614c49ce003f72aa377ac7c8a7fba1ed2f7d6
davidezefosie <davidezefosie@gmail.com>
2024-10-02 09:03:55 +0100

    Build Sort Selector
    Signed-off-by: davidezefosie <davidezefosie@gmail.com>

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,19 +1,24 @@
+import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
+import { BsChevronBarDown } from "react-icons/bs";
+
+// interface Props {
+//     onSort: (platform: Platform) => void;
+//     selectedPlatform: Platform | null;
+//   }
 
 const SortSelector = () => {
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronBarDown />}>
-        {selectedPlatform?.name || "Platforms"}
+        Order by: Relevace
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
-          <MenuItem
-            onClick={() => onSelectPlatform(platform)}
-            key={platform.id}
-          >
-            {platform.name}
-          </MenuItem>
-        ))}
+        <MenuItem>Relevance</MenuItem>
+        <MenuItem>Date added</MenuItem>
+        <MenuItem>Name</MenuItem>
+        <MenuItem>Release date</MenuItem>
+        <MenuItem>Popularity</MenuItem>
+        <MenuItem>Average rating</MenuItem>
       </MenuList>
     </Menu>
   );
