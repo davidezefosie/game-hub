01ddf0eba2a2c852ea93b0901781d882a34741f8
davidezefosie <davidezefosie@gmail.com>
2024-10-02 15:14:56 +0100

    Implement Searching
    Signed-off-by: davidezefosie <davidezefosie@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 
 const App = () => {
@@ -35,7 +36,9 @@ const App = () => {
       }}
     >
       <GridItem area="nav">
-        <NavBar />
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5} paddingTop={9}>
